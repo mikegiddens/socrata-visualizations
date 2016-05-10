4af226bb6cfad2bbd53957734ec808f069eeafa8
Michael Giddens <mikegiddens@silverbiology.com>
2016-05-09 23:55:23 -0600

    initial load

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# socrata-visualizations
+# socrata-visualizations.js (v1.0)
+
 Javascript library to quickly render graphic components based on Socrata as the data source.
+
+# Quick start
+
+  1. Download package...
+  
+  2. Add socrata-visualizations.js and css to your site:
+
+    ```html
+        <link rel="stylesheet" href="../vendors/c3/c3.css" />
+        <script src="./vendors/jquery-1.11.3.min.js"></script>
+        <script src="./vendors/d3.min.js"></script>
+        <script src="./vendors/c3/c3.min.js"></script>
+        <script src="./vendors/soda-js.js"></script>
+        <script src="./socrata-visualizations.js"></script>
+    ```
+
+  3. Create the map and add the layer
+
+    ```javascript
+    pieStationCount = new SV.PieChart('#countStation', {
+        table: '6yvf-kk3n',
+        baseUrl: 'soda.demo.socrata.com'
+    });
+
+    pieStationCount.loadCount('number_of_stations');
+    ```
+
+# Overview
+
+## Classes
+
+* SV - Main class
+* SV.PieChart - Pie chart class
+* SV.Store - Stand alone store class
+
+---------------------------------------
+
+# SV.PieChart
+
+## Properties
+
+### table
+This is the 4x4
+
+## Methods
+
+### loadCount(column)
+This is a shortcut function of store.load() 
+
+### loadSum(column, options)
+
+## Events
+
+---------------------------------------
+
+# SV.Store (Class)
+This is part of each of the visualization components and it can be used independtly
+
+## methods
+
+### load(options, callback)
+
+---------------------------------------
+
+# License MIT
---
+++ socrata-visualizations.js
@@ -0,0 +1,529 @@
+String.prototype.toProperCase = function () {
+    return this.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+};
+
+/* EventManager, v1.0.1
+ *
+ * Copyright (c) 2009, Howard Rauscher
+ * Licensed under the MIT License
+ */
+
+(function () {
+
+    function EventManager() {
+        this._listeners = {};
+    }
+    EventManager.prototype = {
+        addListener: function (name, fn) {
+            (this._listeners[name] = this._listeners[name] || []).push(fn);
+            return this;
+        },
+        removeListener: function (name, fn) {
+            if (arguments.length === 1) { // remove all
+                this._listeners[name] = [];
+            } else if (typeof (fn) === 'function') {
+                var listeners = this._listeners[name];
+                if (listeners !== undefined) {
+                    var foundAt = -1;
+                    for (var i = 0, len = listeners.length; i < len && foundAt === -1; i++) {
+                        if (listeners[i] === fn) {
+                            foundAt = i;
+                        }
+                    }
+
+                    if (foundAt >= 0) {
+                        listeners.splice(foundAt, 1);
+                    }
+                }
+            }
+
+            return this;
+        },
+        fire: function (name, args) {
+            var listeners = this._listeners[name];
+            args = args || [];
+            if (listeners !== undefined) {
+                var data = {},
+                    evt;
+                for (var i = 0, len = listeners.length; i < len; i++) {
+                    evt = new EventManager.EventArg(name, data);
+
+                    listeners[i].apply(window, args.concat(evt));
+
+                    data = evt.data;
+                    if (evt.removed) {
+                        listeners.splice(i, 1);
+                        len = listeners.length;
+                        --i;
+                    }
+                    if (evt.cancelled) {
+                        break;
+                    }
+                }
+            }
+            return this;
+        },
+        hasListeners: function (name) {
+            return (this._listeners[name] === undefined ? 0 : this._listeners[name].length) > 0;
+        }
+    };
+    EventManager.eventify = function (object, manager) {
+        var methods = EventManager.eventify.methods;
+        manager = manager || new EventManager();
+
+        for (var i = 0, len = methods.length; i < len; i++)(function (method) {
+            object[method] = function () {
+                return manager[method].apply(manager, arguments);
+            };
+        })(methods[i]);
+
+        return manager;
+    };
+    EventManager.eventify.methods = ['addListener', 'removeListener', 'fire'];
+
+    EventManager.EventArg = function (name, data) {
+        this.name = name;
+        this.data = data;
+        this.cancelled = false;
+        this.removed = false;
+    };
+    EventManager.EventArg.prototype = {
+        cancel: function () {
+            this.cancelled = true;
+        },
+        remove: function () {
+            this.removed = true;
+        }
+    };
+
+    window.EventManager = EventManager;
+})();
+
+SV = (typeof SV !== 'undefined') ? SV : {};
+
+SV.chart = function () {
+    return this;
+}
+
+SV.chart.prototype.generateChart = function (options) {
+    return c3.generate({
+        bindto: options.bindto,
+        size: options.size,
+        data: options.data,
+        legend: (options.legend ? options.legend : {
+            show: false
+        }),
+        color: (options.color ? {
+            pattern: options.color
+        } : {}),
+        padding: (options.padding) ? options.padding : {
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 0
+        },
+        pie: options.pie ? options.pie : {
+            label: {
+                show: false
+            }
+        },
+        axis: (options.axis ? options.axis : {})
+    });
+}
+
+SV.Store = function (options, parent) {
+    if (options) $.extend(this, {}, options);
+    if(parent) {
+        this._parent = parent;
+    }
+    this.consumer = new soda.Consumer(this.baseUrl);
+    EventManager.eventify(this);
+    return this;
+}
+
+SV.Store.prototype.generateFilter = function (options) {
+    var where = null;
+    if (options) {
+        where = [];
+        options.forEach(function (option) {
+            switch (option.operator) {
+            case '=':
+                where.push([option.field, '=', "'" + option.value + "'"].join(''));
+                break;
+            case 'between':
+                if (option.value && option.value.after && option.value.before) {
+                    where.push(option.field + '>="' + option.value.after + '" AND ' + option.field + '<"' + option.value.before + '"');
+                }
+                break;
+            }
+        })
+    }
+    return where;
+}
+
+SV.Store.prototype.load = function (options, callback) {
+    var self = this;
+    var where = null;
+    if(options.field && self._parent) {
+        self._parent.field = options.field;
+    }
+    if (options.filter) {
+        where = this.generateFilter(options.filter);
+    }
+    if (options.where) {
+        if (where == null) {
+            where = [];
+        }
+        if (typeof options.where == 'string') {
+            where.push(options.where);
+        } else if (typeof options.where == 'object') {
+            for (var k in options.where) {
+                where.push([k, '=', options.where[k]].join(''));
+            }
+        }
+    }
+    var promise = this.consumer.query().withDataset(this.table);
+    if (options.select) {
+        promise = promise.select(options.select);
+    }
+    if (where) {
+        promise = promise.where(where.join(' AND '));
+    }
+    if (options.group) {
+        promise = promise.group(options.group);
+    }
+    if (options.order) {
+        promise = promise.order(options.order);
+    }
+    promise.getRows()
+        .on('success', function (rows) {
+            if (rows) {
+                self.fire('load', [rows]);
+                if (callback) {
+                    callback(rows);
+                }
+            }
+        })
+        .on('error', function (error) {
+            console.error(error);
+        });
+    return this;
+}
+
+SV.Combo = function (el, options) {
+    var self = this;
+    this.el = el;
+    if (options) $.extend(this, {}, options);
+    EventManager.eventify(this);
+    this.store = new SV.Store({
+        field: this.field,
+        table: this.table,
+        baseUrl: this.baseUrl
+    })
+    this.store.addListener('load', function (data) {
+        self.render(data);
+    })
+    return this;
+}
+
+SV.Combo.prototype.render = function (data) {
+    var self = this;
+    if (data) {
+        $(self.el).empty();
+        $(self.el).change('foo', $.proxy(function (data) {
+            this.value = $(this.el + " option:selected").val();
+            this.fire('change', [$(this.el + " option:selected").val()]);
+        }, this));
+        data = _.pluck(data, this.field);
+        $(this.el).append('<option value="' + this.defaultvalue + '" selected>' + this.defaulttext + '</option>');
+        data.forEach(function (cat) {
+            $(self.el).append('<option value="' + cat + '" >' + cat + '</option>');
+        });
+        this.value = $(this.el + " option:selected").val();
+    }
+    return this;
+}
+
+SV.PieChart = function (el, options) {
+    var self = this;
+    this.el = el;
+    this.divisions = 0;
+    this.total = 0;
+    if (options) $.extend(this, {}, options);
+    EventManager.eventify(this);
+    this.chart = new SV.chart();
+    this.store = new SV.Store({
+        field: this.field,
+        table: this.table,
+        baseUrl: this.baseUrl
+    }, this);
+    this.store.addListener('load', function (data) {
+        self.render(data);
+    });
+    return this;
+}
+
+SV.PieChart.prototype._transform = function (data, label) {
+    var output = [];
+    var total = 0;
+    if (label === undefined) {
+        for (var key in data[0]) {
+            if (data[0].hasOwnProperty(key)) {
+                output.push([key.replace(/_/g, " ").toProperCase(), data[0][key]])
+            }
+        }
+        return output
+    } else {
+        $.each(data, function (idx, rec) {
+            rec.count = (typeof rec.count != 'undefined') ? rec.count : 0;
+            if (typeof rec[label] === 'boolean') {
+                output.push([rec[label] ? 'Yes' : 'No', rec.count]);
+            } else {
+                rec[label] = rec[label] + '';
+                output.push([rec[label].toProperCase(), rec.count]);
+            }
+            total += +rec.count;
+        });
+        if (total == 0) {
+            return false;
+        } else {
+            return output;
+        }
+    }
+}
+
+SV.PieChart.prototype._transformNormalize = function (data, groups) {
+    $.each(data, function (idx, rec) {
+        for (group in groups[0]) {
+            rec[groups[0][group]] = Math.round((rec[groups[0][group]] / rec.total) * 100) / 100
+        }
+    });
+    return data;
+}
+
+SV.PieChart.prototype.render = function (data) {
+    var self = this;
+    if (data) {
+        data = this._transform(data, this.field);
+        var optdata = {
+            columns: data,
+            type: 'pie'
+        };
+        (this.colors) ? (optdata.colors = this.colors) : '';
+        (this.color) ? (optdata.color = this.color) : '';
+
+        var opts = {
+            bindto: self.el,
+            data: optdata,
+            pie: this.pie ? this.pie : {
+                label: {
+                    show: false
+                }
+            },
+            size: (this.size) ? this.size : {
+                width: 300,
+                height: 300
+            }
+        };
+        (this.patterncolors) ? (opts.color = this.patterncolors) : '';
+        (this.legend) ? (opts.legend = this.legend) : '';
+        var pie = this.chart.generateChart(opts);
+
+        self.chartObj = pie;
+        var dts = pie.data();
+        self.divisions = dts.length;
+        self.total = 0;
+        dts.forEach(function (dt) {
+            if (dt.values && dt.values[0] && dt.values[0].value) {
+                self.total = self.total + dt.values[0].value;
+            }
+        });
+        /*        
+                var txtData = {};
+                var total = +pie.data.values('Published') + +pie.data.values('Not Published');
+                txtData.total = total;
+                txtData.percentage = Math.round(pie.data.values(this.percentageKey) / total * 100) + '%';
+                txtData.ratio = (pie.data.values(this.percentageKey) ? pie.data.values(this.percentageKey) : 0) + "/" + total + " datasets";
+                self.fire('change', [txtData]);
+        */
+        self.fire('change', []);
+    }
+    return this;
+}
+
+SV.PieChart.prototype.getPercent = function (ky) {
+    var op = null;
+    if (ky && this.chartObj.data()) {
+        this.chartObj.data().forEach(function (dt) {
+            if (ky == dt.id) {
+                if (dt && dt.values && dt.values[0] && dt.values[0].value) {
+                    op = dt.values[0].value;
+                }
+            }
+        });
+    }
+    return Math.round((op / this.total) * 100) + '%';
+}
+
+SV.PieChart.prototype.getValue = function (ky) {
+    var op = null;
+    if (ky && this.chartObj.data()) {
+        this.chartObj.data().forEach(function (dt) {
+            if (ky == dt.id) {
+                if (dt && dt.values && dt.values[0] && dt.values[0].value) {
+                    op = dt.values[0].value;
+                }
+            }
+        });
+    }
+    return op;
+}
+
+SV.PieChart.prototype.loadCount = function (field, options) {
+    var self = this;
+    var storeoptions = {};
+    storeoptions.field = field;
+    storeoptions.group = field;
+    storeoptions.select = [field];
+    storeoptions.select.push( (options && options.aggregatefield)? 'count(' + options.aggregatefield + ') AS count' : 'count(*) AS count');
+    (options && options.where) ? (storeoptions.where = options.where) : '';
+    (options && options.order) ? (storeoptions.order = options.order) : '';
+    (options && options.filter) ? (storeoptions.filter = options.filter) : '';
+    self.store.load(storeoptions);
+}
+
+SV.PieChart.prototype.loadAvg = function (field, options) {
+    var self = this;
+    var storeoptions = {};
+    storeoptions.field = field;
+    storeoptions.group = field;
+    storeoptions.select = [field];
+    storeoptions.select.push( (options && options.aggregatefield)? 'avg(' + options.aggregatefield + ') AS count' : 'avg(*) AS count');
+    (options && options.where) ? (storeoptions.where = options.where) : '';
+    (options && options.order) ? (storeoptions.order = options.order) : '';
+    (options && options.filter) ? (storeoptions.filter = options.filter) : '';
+    self.store.load(storeoptions);
+}
+
+SV.PieChart.prototype.loadSum = function (field, options) {
+    var self = this;
+    var storeoptions = {};
+    storeoptions.field = field;
+    storeoptions.group = field;
+    storeoptions.select = [field];
+    storeoptions.select.push( (options && options.aggregatefield)? 'sum(' + options.aggregatefield + ') AS count' : 'sum(*) AS count');
+    (options && options.where) ? (storeoptions.where = options.where) : '';
+    console.log(storeoptions)
+    (options && options.order) ? (storeoptions.order = options.order) : '';
+    (options && options.filter) ? (storeoptions.filter = options.filter) : '';
+    self.store.load(storeoptions);
+}
+
+SV.BarChart = function (el, options) {
+    var self = this;
+    this.divisions = 0;
+    this.total = 0;
+    this.el = el;
+    if (options) $.extend(this, {}, options);
+    EventManager.eventify(this);
+    this.chart = new SV.chart();
+    this.store = new SV.Store({
+        field: this.field,
+        table: this.table,
+        baseUrl: this.baseUrl
+    }, this)
+    this.store.addListener('load', function (data) {
+        self.render(data);
+    });
+    return this;
+}
+
+SV.BarChart.prototype._transformNormalize = function (data, groups) {
+    $.each(data, function (idx, rec) {
+        for (group in groups[0]) {
+            rec[groups[0][group]] = Math.round((rec[groups[0][group]] / rec.total) * 100) / 100
+        }
+    });
+    return data;
+}
+
+SV.BarChart.prototype.render = function (data) {
+    var self = this;
+    if (this.transformtype) {
+        switch (this.transformtype) {
+            case 'normalize':
+                data = this._transformNormalize(data, this.groups);
+                break;
+            case 'transform':
+                data = this._transform(data, this.field);
+                break;
+        }
+    }
+
+    if (data) {
+        var optdata = {
+            xFormat: this.xFormat,
+            json: data,
+            mimeType: this.mimeType,
+            keys: this.keys,
+            type: 'bar'
+        };
+        (this.colors) ? (optdata.colors = this.colors) : '';
+        (this.names) ? (optdata.names = this.names) : '';
+        (this.groups) ? (optdata.groups = this.groups) : '';
+        var opts = {
+            bindto: this.el,
+            data: optdata,
+            axis: this.axis,
+            size: (this.size) ? this.size : {
+                width: 300,
+                height: 300
+            }
+        };
+
+        var bar = this.chart.generateChart(opts);
+        self.chartObj = bar;
+        var dts = bar.data();
+        self.divisions = dts.length;
+        self.total = 0;
+        dts.forEach(function (dt) {
+            if (dt.values && dt.values[0] && dt.values[0].value) {
+                self.total = self.total + dt.values[0].value;
+            }
+        });
+        self.fire('change', [bar.data]);
+    }
+    return this;
+}
+
+SV.BarChart.prototype.getPercent = function (ky) {
+    var op = null;
+    if (ky && this.chartObj.data()) {
+        this.chartObj.data().forEach(function (dt) {
+            if (ky == dt.id) {
+                if (dt && dt.values && dt.values[0] && dt.values[0].value) {
+                    op = dt.values[0].value;
+                }
+            }
+        });
+
+    }
+    return Math.round((op / this.total) * 100) + '%';
+}
+
+SV.BarChart.prototype.getValue = function (ky) {
+    var op = null;
+    if (ky && this.chartObj.data()) {
+        this.chartObj.data().forEach(function (dt) {
+            if (ky == dt.id) {
+                if (dt && dt.values && dt.values[0] && dt.values[0].value) {
+                    op = dt.values[0].value;
+                }
+            }
+        });
+
+    }
+    return op;
+}
